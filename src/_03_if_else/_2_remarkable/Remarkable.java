e3feecd4af1e745863725efb278781eb27eb951b
root <root@edcacbed425a>
2020-11-07 19:45:45 +0000

    started working on remarkable

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,23 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+String mariene = "She likes to watch Netflex";
+String miranda = "She likes to eat ice cream";
+String jessica = "She likes to play with slime";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String name = JOptionPane.showInputDialog("What is your name?");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
-	}
+//String  = "What is remarkable about that person?";
+/* if the name is mariene, print popup about mariene
+ * else if the name is miranda ....
+ * else if the name is jessica
+ */
+	}y
 }
 
+/* ask for the person's name
+ * then test which name it is and decide what to say about the person
+ *mariene
+*miranda
+jessica
+*/
